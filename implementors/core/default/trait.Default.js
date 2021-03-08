7513162ce4b301b873a9fbc2c3bf9876f7b8a5c7
Tamme Schichler <tamme@schichler.dev>
2021-03-08 13:53:45 +0100

    Added `EventBindingOptions`

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["lignin"] = [{"text":"impl Default for EventBindingOptions","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
