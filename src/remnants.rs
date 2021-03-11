a7e9c8ec0d74b5722298e20463df1effff5d4dcd
Tamme Schichler <tamme@schichler.dev>
2021-03-11 13:06:30 +0100

    Turned RemnantSite into a vacant struct with named fields rather than a vacant enum, since this is what it will be when implemented

--- src/remnants.rs
+++ src/remnants.rs
@@ -1,3 +1,7 @@
 #[allow(clippy::empty_enum)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum RemnantSite {}
+enum Empty {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct RemnantSite {
+	empty: Empty,
+}
